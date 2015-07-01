545d21565ba66fa048ff02d5d47cf5cfe9a40592
fresve <fresve@rr-research.no>
2015-07-01 15:25:17 +0200

    thread pull
    Gets threads from Connect

--- www/js/controllers/Threads.js
+++ www/js/controllers/Threads.js
@@ -1,11 +1,25 @@
 angular.module('starter.controllers')
-	.controller('ThreadsCtrl', ['$scope', function($scope){
-		$scope.threads = [{
-			name: "Something",
-			id: 0,
-		},
-		{
-			name: "Something else",
-			id: 1
-		}];
-	}]);
+    .controller('ThreadsCtrl', [
+        '$scope', '$http', '$log', 'ApiEndpoint', function($scope, $http, $log, ApiEndpoint) {
+            var onError = function(e) {
+                $log.error(e.msg);
+            };
+
+            var transform = function(threads) {
+                $scope.threads = threads.map(function (t) {
+                    var parts = t.links[0].href.split("/");
+                    var id = parts[parts.length - 1];
+                    return angular.extend(t, { "id": id });
+                });
+                $log.info($scope.threads);
+            };
+            var loadThreads = function(id) {
+                $http.get(ApiEndpoint.url + "/forum/"+id).then(function(result) {
+                    $log.info(result);
+                    transform(result.data["forum-threads"]);
+                }, onError);
+            };
+            loadThreads(2);
+            $scope.threads = [];
+        }
+    ]);
