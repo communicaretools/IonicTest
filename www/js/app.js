2adde2962ac04244164326b4c23c2adedb0bd028
frederho <frederikkhols@gmail.com>
2015-07-02 08:25:32 +0200

    default state to login page

--- www/js/app.js
+++ www/js/app.js
@@ -104,5 +104,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
                 templateUrl: 'templates/tabs.html'
             });
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('forum/threads');
+        $urlRouterProvider.otherwise('forum/login');
     });
