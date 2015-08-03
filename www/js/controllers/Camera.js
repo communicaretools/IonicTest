f9f0b2a6686a4e68a48a52c575c02bf97081a16b
frederho <frederikkhols@gmail.com>
2015-08-03 14:55:45 +0200

    Added cordova camara plugin test
    cordova plugin add org.apace.cordova.camera

---
+++ www/js/controllers/Camera.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers')
+	.controller('CameraCtrl',
+	['$scope', 'Camera', function ($scope, Camera){
+		var setPhoto = function(imageURI) {
+			console.log(imageURI);
+			$scope.imageURI = imageURI;
+		};
+
+		var reportError = function(err) {
+			console.err(err);
+		};
+		$scope.getPhoto = function() {
+			Camera.getPicture().then(setPhoto, reportError);
+		};
+	}]);
