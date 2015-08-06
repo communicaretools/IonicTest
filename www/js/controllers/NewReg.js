db10d4cd860b8df2d3e2f70887311d933edc67c5
frederho <frederikkhols@gmail.com>
2015-08-06 16:45:38 +0200

    Registration of smiley cont

--- www/js/controllers/NewReg.js
+++ www/js/controllers/NewReg.js
@@ -1,8 +1,18 @@
 angular.module('starter.controllers')
     .controller('NewRegCtrl', [
         '$scope', '$stateParams', '$state', 'registrationService', function($scope, $stateParams, $state, registrationService) {
-        $scope.newReg = {};
+            $scope.newReg = {
+                text: '',
+                imageIndex: -1
+            };
+            console.log("I have been initialized");
+            $scope.numberOfTimes=function(n){
+                return new Array(n + 1);
+            };
 
+            $scope.selectSmiley = function(index) {
+                $scope.newReg.imageIndex = index;
+            };
             $scope.sendRegistration = function(type) {
                 registrationService.add(type, $scope.newReg, function() {
                     $state.go("home.registration.list");
