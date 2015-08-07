8f762293878e13c97b10c2647a8153d243bd4448
frederho <frederikkhols@gmail.com>
2015-08-07 11:08:45 +0200

    Clean up css and javascript

--- www/js/controllers/NewReg.js
+++ www/js/controllers/NewReg.js
@@ -1,11 +1,7 @@
 angular.module('starter.controllers')
     .controller('NewRegCtrl', [
         '$scope', '$stateParams', '$state', 'registrationService', function($scope, $stateParams, $state, registrationService) {
-            $scope.newReg = {
-                text: '',
-                imageIndex: -1
-            };
-            console.log("I have been initialized");
+            $scope.newReg = {};
             $scope.numberOfTimes=function(n){
                 return new Array(n + 1);
             };
@@ -13,6 +9,7 @@ angular.module('starter.controllers')
             $scope.selectSmiley = function(index) {
                 $scope.newReg.imageIndex = index;
             };
+
             $scope.sendRegistration = function(type) {
                 registrationService.add(type, $scope.newReg, function() {
                     $state.go("home.registration.list");
