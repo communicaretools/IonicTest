300ae1bc4410c4c6f87c0ca27869d614d6b7db82
frederho <frederikkhols@gmail.com>
2015-08-07 14:49:24 +0200

    Revert "Mark active tab element"
    This reverts commit e4823533bec8f537c65398b4cb7b3d0ef9747d83.

--- www/js/controllers/Menu.js
+++ www/js/controllers/Menu.js
@@ -1,50 +1,12 @@
 angular.module('starter.controllers')
 	.controller('MenuCtrl', ['$scope', 'loginManager', function ($scope, loginManager) {
-        $scope.activeState = "";
-        $scope.menuItems = [
-            {
-                state: "home.forum",
-                name:"Forum"
-            },
-            {
-                state: "home.registration.list",
-                name:"Registreringer"
-            },
-            {
-                state: "home.account",
-                name:"Kontoinnstillinger"
-            },
-            {
-                state: "home.messages",
-                name:"Meldinger"
-            },
-            {
-                state: "home.camera",
-                name:"Kamera"
-            },
-            {
-                state: "home.video",
-                name:"Video"
-            },
-            {
-                state: "home.login",
-                name: "Logg ut"
-            }
-        ];
-
         if (typeof $scope.isLoggedIn === "undefined") {
             loginManager.options(function(data) {
                 $scope.isLoggedIn = data.isLoggedIn;
-            });
-        };
-        $scope.$on("logInChange", function (e, status) {
-            $scope.isLoggedIn = status;
-            }
-        );     
-
-        $scope.setActiveState = function(state) {
-            $scope.activeState = state;
-        };
-
-
+            })
+        }
+	    $scope.$on("logInChange", function (e, status) {
+        console.log(status);
+        $scope.isLoggedIn = status;
+    });
 }]);
