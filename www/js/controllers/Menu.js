3049a3d394548b74aa74ad2355f39f3454c4f983
Yizhak Itzchaki <Yizhak.Itzchaki@rr-research.no>
2015-08-07 14:47:09 +0200

    Adjust menu

--- www/js/controllers/Menu.js
+++ www/js/controllers/Menu.js
@@ -1,5 +1,35 @@
 angular.module('starter.controllers')
-	.controller('MenuCtrl', ['$scope', 'loginManager', function ($scope, loginManager) {
+	.controller('MenuCtrl', ['$scope', 'loginManager', '$state', function ($scope, loginManager, $state) {
+
+
+        $scope.menuItems=[
+            {
+                state: "forum",
+                name: "Forum"},
+            {
+                state: "registration",
+                name: "Registreringer"},
+            {
+                state: "account",
+                name: "Instillnger",
+            },
+            {
+                state: "messages",
+                name: "Meldinger"},
+            {
+                state: "camera",
+                name: "Kamera"},
+            {
+                state: "video",
+                name:"Video"},
+            {
+                state: "login",
+                name:"Logg ut"},
+        ]
+
+        
+        console.log($scope.menuItems);
+
         if (typeof $scope.isLoggedIn === "undefined") {
             loginManager.options(function(data) {
                 $scope.isLoggedIn = data.isLoggedIn;
@@ -8,5 +38,13 @@ angular.module('starter.controllers')
 	    $scope.$on("logInChange", function (e, status) {
         console.log(status);
         $scope.isLoggedIn = status;
+
+        
+        $activeState="";
+
+        $scope.setActiveState=function (stat) {
+            activeState=stat;
+            console.log("test");
+        }
     });
 }]);
