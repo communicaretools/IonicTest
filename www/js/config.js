2a09ed67501c97dc6fb8d879051303805e088389
fresve <fresve@rr-research.no>
2015-08-13 09:42:10 +0200

    changed config.js back to debug url

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 angular.module('starter.config', []).constant("ApiEndpoint", {
-        //url: "http://localhost:8100/api" // for localhost debugging
-        url: "http://sps.rr-research.no/test/reconnect-pretest/webchoice/api"
+        url: "http://localhost:8100/api" // for localhost debugging
+        //url: "http://sps.rr-research.no/test/reconnect-pretest/webchoice/api"
         //url: "http://sps.rr-research.no/demo/connectwcp/webchoice/api"
         //url: "http://10.163.2.45:8080/api" // For running on device, add correct IP and port
     })
