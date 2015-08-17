aa2278f942a880e1a61762665daa609fb38bd8ca
Joao Delgado <joadel@rr-research.no>
2015-08-17 15:59:11 +0200

    Add cordovaCalendar to project; continue on calend

--- www/js/controllers/Calendar.js
+++ www/js/controllers/Calendar.js
@@ -1,10 +1,44 @@
 angular.module('starter.controllers')
 	.controller('CalendarCtrl', [
 		'$scope',
-		'$localStorage',
-		function ($scope, $localStorage) {
-			if(!$scope.user) {
-				$scope.user = $localStorage.user;
-			}
+		'$log',
+		'$cordovaCalendar',
+		function ($scope, $log, $cordovaCalendar) {
+			$scope.options = {};
+
+			var onError = function (e) {
+				$log.error(e.msg);
+			};
+
+			$scope.addEvent = function (options) {
+				$log.log("Creating event...");
+				$cordovaCalendar.createEventInteractively(options)
+					.then(function(result) {
+						$log.log(result);
+					}, function(err) {
+						onError(err);
+					});
+			};
+
+			$scope.clear = function() {
+				$scope.options = {};
+			};
+
 		}
 	]);
+
+
+
+	/*
+ $cordovaCalendar.createEventInteractively({
+    title: 'Space Race',
+    location: 'The Moon',
+    notes: 'Bring sandwiches',
+    startDate: new Date(2015, 0, 6, 18, 30, 0, 0, 0),
+    endDate: new Date(2015, 1, 6, 12, 0, 0, 0, 0)
+  }).then(function (result) {
+    // success
+  }, function (err) {
+    // error
+  });
+	*/
