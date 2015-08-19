cb7c5ce349a468826b78649fc45b033681e600ab
fresve <fresve@rr-research.no>
2015-08-19 13:01:17 +0200

    started on profile/avatar

--- www/js/Services/profileManager.js
+++ www/js/Services/profileManager.js
@@ -1,14 +1,37 @@
 angular.module('starter.services')
     .factory("profileManager", [
-        "$http", "$log", "ApiEndpoint", function ($http, $log, ApiEndpoint) {
+        "$http",
+        "$log",
+        "ApiEndpoint", function ($http, $log, ApiEndpoint) {
+            var onError = function (e) {
+                $log.error(e.msg);
+            };
 
-            var getUser = function (onSuccess, onError) {
+            var getUser = function (onSuccess) {
                 $http.get(ApiEndpoint.url + "/profile/userdata")
                     .then(onSuccess, onError);
             };
 
+            var getAvatar = function(onSuccess) {
+                $http.get(ApiEndpoint.url + "/profile/avatar")
+                    .then(onSuccess, onError);
+            }
+
+            var avatarList = function(onSuccess) {
+                $http.get(ApiEndpoint.url + "/profile/avatarList")
+                    .then(onSuccess, onError);
+            }
+            
+            var saveAvatar = function (avatar, onSuccess) {
+                $http.post(ApiEndpoint.url + "/profile/avatarList", { 'avatar': avatar })
+                    .then(onSuccess, onError);
+            }
+
             return {
                 "get": getUser,
+                "getAvatar": getAvatar,
+                "saveAvatar": saveAvatar,
+                "avatarList": avatarList
             };
         }
     ]);
--- www/js/controllers/Account.js
+++ www/js/controllers/Account.js
@@ -1,11 +1,46 @@
 angular.module('starter.controllers')
     .controller('AccountCtrl', [
-        '$scope', 'preferenceService',
-        function ($scope, preferenceService) {
+        '$scope',
+        '$localStorage',
+        '$ionicModal',
+        'preferenceService',
+        'profileManager',
+        function ($scope, $localStorage, $ionicModal, preferenceService, profileManager) {
             $scope.dateFormat = preferenceService.getDateFormat();
+            $scope.avatar = $localStorage.user.avatar;
 
-            $scope.updateFormat = function(format) {
+            $scope.updateFormat = function (format) {
                 preferenceService.updateFormat(format);
             };
+
+            $scope.selectAvatar = function(selected) {
+                profileManager.saveAvatar(selected, function () {
+                    profileManager.getAvatar(function(result) {
+                        $localStorage.user.avatar = result.data;
+                        $scope.avatar = result.data;
+                    });
+                    $scope.closeModal();
+                });
+            };
+
+            $scope.openModal = function () {
+                profileManager.avatarList(function(result) {
+                    $scope.modal.list = result.data.avatars;
+                });
+                $scope.modal.show();
+            };
+            $scope.closeModal = function () {
+                $scope.modal.hide();
+            };
+            $scope.$on('$destroy', function () {
+                $scope.modal.remove();
+            });
+
+            $ionicModal.fromTemplateUrl('avatar-modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                $scope.modal = modal;
+            });
         }
     ]);
